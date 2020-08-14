316e743e80546efaa0e304cbf1ed67e370e108cb
Luan Jesus <luan.todos@gmail.com>
2020-08-14 10:15:50 -0300

    Capitulo 2 parte 1

---
+++ 2_caracteres/OlaRegex.js
@@ -0,0 +1,9 @@
+const texto = 'Casa bonita é uma casa amarela da esquina com a rua ACASALAR.';
+
+const regex = /casa/gi
+
+// Busca literal
+
+console.log(texto.match(regex));
+
+console.log(texto.match(/a b/));
